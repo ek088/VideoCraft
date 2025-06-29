1ddb0aba54ba07f5c95e22d118f01e9c0c9aa97f
Alexander Smolyak <ezgtk0@gmail.com>
2025-06-29 12:51:12 +0000

    fix: center view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        {children}
+        <div className="max-w-screen-lg mx-auto px-4">
+          {children}
+        </div>
         <Toaster />
       </body>
     </html>
